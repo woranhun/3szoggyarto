2d71d1a3e059988140f5554b6435dc413965660b
Blaskó <esamble01@gmail.com>
2018-04-25 18:41:51 +0200

    hasd

--- program.py
+++ program.py
@@ -1,1 +1,6 @@
+def DrawMiddleTriangle(n):
+	for i in range(n+1):
+		print((n-i) * " " + i * "x" + (i-1) * "x")
 
+n = int(input("Kérlek add meg a háromszög oldalainak a hosszát! "))
+MiddleTriangle(n)
